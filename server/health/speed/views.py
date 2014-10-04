1d45824e04a27cae51c1a979f4550ade7c2fcfa9
GurjitSingh17 <gurjit.singh@berkeley.edu>
2014-10-04 08:56:37 -0700

    all routes configured

--- server/health/speed/views.py
+++ server/health/speed/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 # Create your views here.
-def get_request(request,file_path):
-	response_data = {'text':"hello world this is a test "+file_path}
+def test_get(request):
+	response_data = {'text':"hello world this is a test "}
 	return HttpResponse(json.dumps(response_data),content_type="application/json")
 
+def get_article(request,article_id):
+	response_data = {'text':"hello world this is a test of id "+ article_id}
+	return HttpResponse(json.dumps(response_data),content_type="application/json")
+
+@csrf_exempt
+def post_article(request):
+	response_data = {'text':"hello world this is a test of post"}
+	return HttpResponse(json.dumps(response_data),content_type="application/json")
