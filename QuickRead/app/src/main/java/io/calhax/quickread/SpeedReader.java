9157e687f49b8888efd618759a899f0b1377a69a
James Carr <jcarr@berkeley.edu>
2014-10-04 07:12:08 -0700

    working fast local word switching

--- QuickRead/app/src/main/java/io/calhax/quickread/SpeedReader.java
+++ QuickRead/app/src/main/java/io/calhax/quickread/SpeedReader.java
@@ -4,14 +4,47 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.os.Handler;
 
 
 public class SpeedReader extends Activity {
+
+    TextView flash_me;
+    int WPM=600;
+    int index = 0;
+    String[] words = "This is a test at 600 words per minute of the glass speed reading app".split(" ");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed_reader);
+        flash_me = (TextView)findViewById(R.id.flash);
+        //flash_me.setText("EXCEEDINGMAXCHARLIMIT");
+    }
+
+    private Handler handle = new Handler();
+    private Runnable timer = new Runnable() {
+        @Override
+        public void run() {
+            if (index < words.length) {
+                flash_me.setText(words[index]);
+                index++;
+            }
+            handle.postDelayed(timer, 60000/WPM);
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handle.post(timer);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        handle.removeCallbacks(timer);
     }
 
 
