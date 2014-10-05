23c01c7d9486348d4c54f27ec03086aea927dd6d
Paul Moulton <pmoulton2@gmail.com>
2014-10-05 01:41:00 -0700

    Initial Commit

---
+++ SpeedRead/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpeedRead
+//
+//  Created by Paul on 10/5/14.
+//  Copyright (c) 2014 Aperture Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpeedRead/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpeedRead
+//
+//  Created by Paul on 10/5/14.
+//  Copyright (c) 2014 Aperture Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
