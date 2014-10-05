5478b073b5395e76b32fb3b1bbe444010db9bbf4
GurjitSingh17 <gurjit.singh@berkeley.edu>
2014-10-04 20:14:08 -0700

    more func

--- server/health/speed/models.py
+++ server/health/speed/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime 
 
 class Article(models.Model):
 	title = models.CharField(max_length=128)
-	file_path = models.CharField(max_length=128)
+	has_text = models.BooleanField(default=False)
+	text = models.TextField(default="No Content")
+	date_uploaded = models.DateField(default=datetime.now())
+
+	def __unicode__(self):
+		return self.title
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     articles = models.ManyToManyField(Article)
+
+    def __unicode__(self):
+    	return user.username
 
 
--- server/health/speed/views.py
+++ server/health/speed/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate
 from models import *
+from file_util import *
 import json
 
 # Create your views here.
@@ -11,11 +12,23 @@ def test_get(request):
 	return HttpResponse(json.dumps(response_data),content_type="application/json")
 
 def get_article(request,article_id):
-	response_data = {'text':"hello world this is a test of id "+ article_id}
+	response_data = {'error':"invalid id"}
+	try:
+		article_id = int(article_id)
+	except ValueError as e:
+		return HttpResponse(json.dumps(response_data),content_type="application/json",status=400)
+	if article_id == 0 or article_id > len(Article.objects.all()):
+		return HttpResponse(json.dumps(response_data),content_type="application/json",status=400)
+	article = Article.objects.get(id=article_id)
+	if not article.has_text:
+		response_data = {'error':"no content"}
+		return HttpResponse(json.dumps(response_data),content_type="application/json",status=400)
+	article_txt = article.text
+	response_data = {'text':article_txt}
 	return HttpResponse(json.dumps(response_data),content_type="application/json")
 
 @csrf_exempt
-def post_article(request):
+def get_all_user_articles(request):
 	if request.method == "GET":
 		return HttpResponse(status=400)
 	username = request.POST['username']
@@ -24,7 +37,30 @@ def post_article(request):
 	user = authenticate(username=username,password=password)
 	if user:
 		profile = UserProfile.objects.get(user=user)
-		article = Article(title="some title",file_path="some file path")
+		articles = []
+		response_data = {"articles":articles}
+		for article in profile.articles.all():
+			article_json = {"title":article.title,"text":article.text}
+			articles.append(article_json)
+		return HttpResponse(json.dumps(response_data),content_type="application/json",status=200)
+	else:
+		response_data = {'text':"authentication failed"}
+		return HttpResponse(json.dumps(response_data),content_type="application/json",status=400)
+
+
+@csrf_exempt
+def post_article(request):
+	if request.method == "GET":
+		return HttpResponse(status=400)
+	username = request.POST['username']
+	password = request.POST['password']
+	url      = request.POST['url']
+
+	user = authenticate(username=username,password=password)
+	if user:
+		profile = UserProfile.objects.get(user=user)
+		article = Article(title="some title",text="some text")
+		article.has_text = True
 		article.save()
 		profile.articles.add(article)
 		profile.save()
@@ -32,7 +68,7 @@ def post_article(request):
 		response_data = {'text':"hello world this is a test of post " + t1}
 		return HttpResponse(json.dumps(response_data),content_type="application/json",status=200)
 	
-	response_data = {'text':"content not added"}
+	response_data = {'text':"authentication failed"}
 	return HttpResponse(json.dumps(response_data),content_type="application/json",status=400)
 
 @csrf_exempt
