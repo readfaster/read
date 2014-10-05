8537c8f26a1e8a38cf58bc696122e4d5d6de0a83
Paul Moulton <pmoulton2@gmail.com>
2014-10-04 19:51:29 -0700

    Initial Commit

---
+++ Reading Glass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reading Glass
+//
+//  Created by Paul on 10/4/14.
+//  Copyright (c) 2014 Aperture Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reading GlassTests/Reading_GlassTests.m
@@ -0,0 +1,40 @@
+//
+//  Reading_GlassTests.m
+//  Reading GlassTests
+//
+//  Created by Paul on 10/4/14.
+//  Copyright (c) 2014 Aperture Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Reading_GlassTests : XCTestCase
+
+@end
+
+@implementation Reading_GlassTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
