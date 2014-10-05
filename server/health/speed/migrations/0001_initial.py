a14a40d38bf29ca2503dc9850ca3a3d46ef0fcf3
Allen Li <allen@allen.li>
2014-10-05 12:03:40 +0000

    add resolved migration

---
+++ server/health/speed/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=128)),
+                ('has_text', models.BooleanField(default=False)),
+                ('text', models.TextField(default=b'No Content')),
+                ('date_uploaded', models.DateField(default=datetime.datetime(2014, 10, 5, 2, 33, 25, 85252))),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('articles', models.ManyToManyField(to='speed.Article')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
